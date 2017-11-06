d3ced922fcb0294d3dedc710914ac70fecaf49e3
fsalmeida <fabricio.sda@gmail.com>
2017-11-05 22:59:10 -0200

    Inserindo tela inicial
    Inserindo tela inicial e tratamento de teclas.

--- FroggerGame/MonoGame2D/Game1.cs
+++ FroggerGame/MonoGame2D/Game1.cs
@@ -14,6 +14,12 @@ namespace MonoGame2D
         float screenWidth;
         float screenHeight;
         Texture2D background;
+        bool gameStarted;
+        bool gameOver;
+        int score;
+        int lives;
+        SpriteFont stateFont;
+        Texture2D startGameSplash;
 
 
         public Game1()
@@ -26,6 +32,12 @@ namespace MonoGame2D
         protected override void Initialize()
         {
             base.Initialize();
+
+            // Iniciaza parametros de jogo
+            gameOver = false;
+            gameStarted = false;
+            lives = 0;
+            score = 0;
 
             // Inicializa escala de frames da tela utilizada
             // Inicializa em tela cheia
@@ -44,6 +56,10 @@ namespace MonoGame2D
 
             // Carrega textura de background do jogo
             background = Content.Load<Texture2D>("background");
+            startGameSplash = Content.Load<Texture2D>("start-splash");
+
+            // Carrega estilo de fontes
+            stateFont = Content.Load<SpriteFont>("GameState");
         }
 
         // Metodo de descarga de elementos externos
@@ -55,6 +71,7 @@ namespace MonoGame2D
         protected override void Update(GameTime gameTime)
         {
             float elapsedTime = (float)gameTime.ElapsedGameTime.TotalSeconds;
+            KeyboardHandler();
             base.Update(gameTime);
         }
 
@@ -69,6 +86,26 @@ namespace MonoGame2D
             // Desenha o background carregado
             spriteBatch.Draw(background, new Rectangle(0, 0, (int)screenWidth, (int)screenHeight), Color.White);
 
+            // Se o jogo ainda não começou fica em tela de inicio
+            if (!gameStarted)
+            {
+                // Carrega tela preta no inicio de jogo aguardando um espaço para iniciar
+                spriteBatch.Draw(startGameSplash, new Rectangle(0, 0,
+                (int)screenWidth, (int)screenHeight), Color.White);
+                String title = "FROGGER - THE MEDIEVAL EDITION";
+                String pressSpace = "Press Space to start";
+                // Insere temanho de fonte para escrita
+                Vector2 titleSize = stateFont.MeasureString(title);
+                Vector2 pressSpaceSize = stateFont.MeasureString(pressSpace);
+                // Escreve centralizado
+                spriteBatch.DrawString(stateFont, title,
+                new Vector2(screenWidth / 2 - titleSize.X / 2, screenHeight / 3),
+                Color.ForestGreen);
+                spriteBatch.DrawString(stateFont, pressSpace,
+                new Vector2(screenWidth / 2 - pressSpaceSize.X / 2,
+                screenHeight / 2), Color.White);
+            }
+
             spriteBatch.End();
             // Encerra o ambente de operações de desenho na tela
 
@@ -81,6 +118,56 @@ namespace MonoGame2D
             DisplayInformation d = DisplayInformation.GetForCurrentView();
             f *= (float)d.RawPixelsPerViewPixel;
             return f;
+        }
+
+        // Metodo de leitura do teclado
+        void KeyboardHandler()
+        {
+            KeyboardState state = Keyboard.GetState();
+
+            // Encerra o jogo se a tecla esc for precionada
+            if (state.IsKeyDown(Keys.Escape))
+            {
+                Exit();
+            }
+
+            // Inicia o jogo de a tecla espaço for precionada.
+            if (!gameStarted)
+            {
+                if (state.IsKeyDown(Keys.Space))
+                {
+                    StartGame();
+                    gameStarted = true;
+                    gameOver = false;
+                }
+                return;
+            }
+            
+            // Controla teclas de direção
+            if (state.IsKeyDown(Keys.Left))
+            {
+
+            }
+            else if (state.IsKeyDown(Keys.Right))
+            {
+
+            }
+            else if (state.IsKeyDown(Keys.Up))
+            {
+
+            }
+            else if (state.IsKeyDown(Keys.Down))
+            {
+
+            }
+        }
+
+        // Metodo de inicio do jogo
+        public void StartGame()
+        {
+            // Inicializa as variaveis de jogo
+            lives = 5;
+            score = 0;
         }
     }
 }

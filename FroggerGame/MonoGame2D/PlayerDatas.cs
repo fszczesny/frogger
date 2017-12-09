938487e8a4832a376d9e33ee8970d22771f0b049
fsalmeida <fabricio.sda@gmail.com>
2017-12-08 23:34:13 -0200

    Inserindo clases de controle do historico de jogadores
    Inserindo clases de controle do historico de jogadores + arquivo de controle e funções de manipulação de arquivos .xml

---
+++ FroggerGame/MonoGame2D/PlayerDatas.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+using Windows.Graphics.Display;
+using Windows.UI.ViewManagement;
+using System.Threading.Tasks;
+
+namespace MonoGame2D
+{
+    public class PlayerDatas
+    {
+        private string name;
+        private int points;
+
+        public PlayerDatas()
+        {
+
+        }
+
+        public string getName()
+        {
+            return this.name;
+        }
+
+        public int getPoints()
+        {
+            return this.points;
+        }
+
+        public void setName(string newName)
+        {
+            this.name = newName;
+        }
+
+        public void setPoints(int newPoints)
+        {
+            this.points = newPoints;
+        }
+
+        ~PlayerDatas()
+        {
+
+        }
+    }
+}
